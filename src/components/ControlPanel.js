39a47839eb6662a2f785d44727c96b2c3dd5490d
daibi07 <1047776488@qq.com>
2020-07-16 03:05:22 +0800

    控制视图修改完成；所有地区的数据全部导入；图表标题自动更新

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -5,63 +5,111 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-// import { FixedSizeList } from 'react-window';
+import { FixedSizeList } from 'react-window';
 
-// const useStyles = makeStyles((theme) => ({
-//     root: {
-//       width: '100%',
-//       height: 400,
-//       maxWidth: 300,
-//       backgroundColor: theme.palette.background.paper,
-//     },
-//   }));
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    height: 900,
+    maxWidth: 300,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+
+
+export default function VirtualizedList() {
+  const classes = useStyles();
+  const {dispatch} = useContext(store);
+
+  function renderRow(props) {
+    const { index, style } = props;
+    const RegionArray=["Shanghai","Beijing","New York","Hawaii","British Columbia","Gibraltar","Spain","Singapore","Ireland",
+    "Abruzzo","Amazonas","Fujian","Gabon","Serbia","Acre","Afghanistan","Alabama","Alagoas","Alaska","Albania","Alberta","Algeria",
+    "Amapa","Andorra","Angola","Anguilla","Anhui","Argentina","Arizona","Arkansas",
+    "Aruba","Australian Capital Territory","Austria","Azerbaijan","Bahia","Bahrain","Bangladesh",
+    "Barbados","Basilicata","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan",
+    "Bolivia","Bonaire, Sint Eustatius and Saba","Bosnia and Herzegovina","Botswana",
+    "British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burma","Burundi","Cabo Verde","Calabria",
+    "California","Cambodia","Cameroon","Campania","Cayman Islands","Ceara","Central African Republic","Chad",
+    "Channel Islands","Chile","Chongqing","Colombia","Colorado","Congo (Brazzaville)","Congo (Kinshasa)","Connecticut",
+    "Costa Rica","Croatia","Cuba","Curacao","Cyprus","Czechia","Delaware","Denmark","District of Columbia",
+    "Distrito Federal","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Emilia-Romagna",
+    "Eritrea","Espirito Santo","Estonia","Eswatini","Ethiopia","Faroe Islands","Fiji","Finland","Florida","France",
+    "French Guiana","French Polynesia","Friuli Venezia Giulia","Gambia","Gansu","Georgia","Georgia",
+    "Germany","Ghana","Goias","Greece","Greenland","Guangdong","Guangxi","Guatemala","Guinea","Guizhou","Guyana",
+    "Hainan","Haiti","Hebei","Heilongjiang","Henan","Honduras","Hong Kong","Hubei","Hunan","Hungary","Iceland",
+    "Idaho","Illinois","India","Indiana","Indonesia","Inner Mongolia","Iowa","Iran","Iraq","Isle of Man","Israel",
+    "Jamaica","Japan","Jiangsu","Jiangxi","Jilin","Jordan","Kansas","Kazakhstan","Kentucky","Kenya","Korea, South","Kuwait",
+    "Kyrgyzstan","Latvia","Lazio","Lebanon","Liaoning","Liberia","Libya","Liguria","Lithuania","Lombardia","Louisiana",
+    "Luxembourg","Macau","Madagascar","Maine","Malawi","Malaysia","Mali","Manitoba","Maranhao","Marche","Maryland",
+    "Massachusetts","Mato Grosso","Mato Grosso Do Sul","Mauritania","Mauritius","Mexico","Michigan","Minas Gerais","Minnesota",
+    "Mississippi","Missouri","Moldova","Molise","Mongolia","Montana","Montserrat","Morocco","Mozambique","Namibia","Nebraska",
+    "Nepal","Netherlands","Nevada","New Brunswick","New Caledonia","New Hampshire","New Jersey","New Mexico","New South Wales",
+    "New Zealand","Newfoundland and Labrador","Nicaragua","Niger","Nigeria","Ningxia","North Carolina","North Dakota",
+    "Northern Territory","Northwest Territories","Norway","Nova Scotia","Ohio","Oklahoma","Oman","Ontario","Oregon","P.A. Bolzano",
+    "P.A. Trento","Pakistan","Panama","Papua New Guinea","Para","Paraguay","Paraiba","Parana","Pennsylvania","Pernambuco","Peru",
+    "Philippines","Piaui","Piemonte","Poland","Portugal","Prince Edward Island","Puglia","Qatar","Qinghai","Quebec","Queensland",
+    "Reunion","Rio De Janeiro","Rio Grande Do Norte","Rio Grande Do Sul","Romania","Rondonia","Roraima","Russia","Rwanda",
+    "Saint Barthelemy","Saint Pierre and Miquelon","Santa Catarina","Sao Paulo","Sardegna","Saskatchewan","Saudi Arabia",
+    "Senegal","Serbia","Sergipe","Seychelles","Shaanxi","Shandong","Shanxi","Sichuan","Sicilia","Sierra Leone",
+    "Sint Maarten","Slovakia","Slovenia","Somalia","South Africa","South Australia","South Carolina","South Dakota",
+    "South Sudan","Sri Lanka","Sudan","Suriname","Sweden","Switzerland","Syria","Taiwan","Tanzania","Tasmania","Tennessee",
+    "Texas","Thailand","Tianjin","Tibet","Timor-Leste","Tocantins","Togo","Toscana","Trinidad and Tobago","Tunisia","Turkey",
+    "Turks and Caicos Islands","Uganda","Ukraine","Umbria","United Arab Emirates","United Kingdom","Uruguay","Utah","Uzbekistan",
+    "Valle d'Aosta","Veneto","Venezuela","Vermont","Victoria","Vietnam","Virginia","Washington","West Virginia","Western Australia",
+    "Wisconsin","Wyoming","Xinjiang","Yukon","Yunnan","Zambia","Zhejiang","Zimbabwe"]
+    
+    //another array contain country name
+
+    return (<div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'}  
+            style={style} >
+                <ListItem button key={index} 
+                onClick={() => dispatch({type: RegionArray[index]})}>
+                    <ListItemText primary={`${RegionArray[index]}`} />  
+                </ListItem>
+            </div>
+    );  
+  }
   
-// function renderRow(props) {
-//     const { index, style } = props;
-  
-//     return (
-//       <ListItem button style={style} key={index}>
-//         <ListItemText primary={`Item ${index + 1}`} />
-//       </ListItem>
-//     );
-// }
-  
-// renderRow.propTypes = {
-//     index: PropTypes.number.isRequired,
-//     style: PropTypes.object.isRequired,
-// };
+  renderRow.propTypes = {
+    index: PropTypes.number.isRequired,
+    style: PropTypes.object.isRequired,
+  };
 
-// export default function VirtualizedList() {
-//     const classes = useStyles();
-
-//     return (
-//         <div className={classes.root}>
-//             <FixedSizeList height={400} width={300} itemSize={46} itemCount={200}>
-//                 {renderRow}
-//             </FixedSizeList>
-//         </div>
-//     );
-// }
-
-
-function ControlPanel() {
-    const {dispatch} = useContext(store);
-    return <div>
-        <button className="myButton" onClick={() => dispatch({type: 'Shanghai'})}>Shanghai, China</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Beijing'})}>Beijing, China</button>
-        <button className="myButton" onClick={() => dispatch({type: 'New York'})}>New York, U.S.A</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Hawaii'})}>Hawaii, U.S.A</button>
-        <button className="myButton" onClick={() => dispatch({type: 'British Columbia'})}>British Columbia, Canada</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Gibraltar'})}>Gibraltar, U.K</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Spain'})}>Spain, Spain</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Singapore'})}>Singapore, Singapore</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Ireland'})}>Ireland, Ireland</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Abruzzo'})}>Abruzzo, Italy</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Amazonas'})}>Amazonas, Brazil</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Fujian'})}>Fujian, China</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Gabon'})}>Gabon, Gabon</button>
-        <button className="myButton" onClick={() => dispatch({type: 'Serbia'})}>Serbia, Serbia</button>
-    </div>;
+  return (
+    <div className={classes.root}>
+      <FixedSizeList height={300} width={200} itemSize={36} itemCount={325}>
+        {renderRow}
+      </FixedSizeList>
+    </div>
+  );
 }
 
-export default ControlPanel;
+
+
+
+// function ControlPanel() {
+//     const {dispatch} = useContext(store);
+//     return <div>
+//         <ul>
+//         <li className="myButton" onClick={() => dispatch({type: 'Shanghai'})}>Shanghai, China</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Beijing'})}>Beijing, China</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'New York'})}>New York, U.S.A</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Hawaii'})}>Hawaii, U.S.A</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'British Columbia'})}>British Columbia, Canada</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Gibraltar'})}>Gibraltar, U.K</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Spain'})}>Spain, Spain</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Singapore'})}>Singapore, Singapore</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Ireland'})}>Ireland, Ireland</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Abruzzo'})}>Abruzzo, Italy</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Amazonas'})}>Amazonas, Brazil</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Fujian'})}>Fujian, China</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Gabon'})}>Gabon, Gabon</li>
+//         <li className="myButton" onClick={() => dispatch({type: 'Serbia'})}>Serbia, Serbia</li>
+//         </ul>
+//     </div>;
+// }
+
+// export default ControlPanel;
--- src/store/index.js
+++ src/store/index.js
@@ -24,9 +24,60 @@ const initialState = {
 // 不同请求的处理
 const reducer = (state, action) => {
     switch (action.type) {
-        case 'Abruzzo': case 'Amazonas': case 'Fujian': case 'Gabon': case 'Serbia': case 'Shanghai':
-        case 'Beijing': case 'Beijing': case 'New York': case 'Hawaii': case 'British Columbia': 
-        case 'Gibraltar': case 'Spain': case 'Singapore': case 'Ireland':
+        case "Abruzzo": case "Acre": case "Afghanistan": case "Alabama": case "Alagoas": case "Alaska": 
+        case "Albania": case "Alberta": case "Algeria": case "Amapa": case "Amazonas": case "Andorra": 
+        case "Angola": case "Anguilla": case "Anhui": case "Argentina": case "Arizona": case "Arkansas": 
+        case "Aruba": case "Australian Capital Territory": case "Austria": case "Azerbaijan": case "Bahia": 
+        case "Bahrain": case "Bangladesh": case "Barbados": case "Basilicata": case "Beijing": case "Belarus": 
+        case "Belgium": case "Belize": case "Benin": case "Bermuda": case "Bhutan": case "Bolivia": 
+        case "Bonaire, Sint Eustatius and Saba": case "Bosnia and Herzegovina": case "Botswana": 
+        case "British Columbia": case "British Virgin Islands": case "Brunei": case "Bulgaria": 
+        case "Burkina Faso": case "Burma": case "Burundi": case "Cabo Verde": case "Calabria": 
+        case "California": case "Cambodia": case "Cameroon": case "Campania": case "Cayman Islands": 
+        case "Ceara": case "Central African Republic": case "Chad": case "Channel Islands": case "Chile": 
+        case "Chongqing": case "Colombia": case "Colorado": case "Congo (Brazzaville)": case "Congo (Kinshasa)": 
+        case "Connecticut": case "Costa Rica": case "Croatia": case "Cuba": case "Curacao": case "Cyprus": 
+        case "Czechia": case "Delaware": case "Denmark": case "District of Columbia": case "Distrito Federal": 
+        case "Djibouti": case "Dominica": case "Dominican Republic": case "Ecuador": case "Egypt": case "El Salvador": 
+        case "Emilia-Romagna": case "Eritrea": case "Espirito Santo": case "Estonia": case "Eswatini": case "Ethiopia": 
+        case "Faroe Islands": case "Fiji": case "Finland": case "Florida": case "France": case "French Guiana": 
+        case "French Polynesia": case "Friuli Venezia Giulia": case "Fujian": case "Gabon": case "Gambia": case "Gansu": 
+        case "Georgia": case "Georgia": case "Germany": case "Ghana": case "Gibraltar": case "Goias": case "Greece": 
+        case "Greenland": case "Guangdong": case "Guangxi": case "Guatemala": case "Guinea": case "Guizhou": case "Guyana": 
+        case "Hainan": case "Haiti": case "Hawaii": case "Hebei": case "Heilongjiang": case "Henan": case "Honduras": 
+        case "Hong Kong": case "Hubei": case "Hunan": case "Hungary": case "Iceland": case "Idaho": case "Illinois": 
+        case "India": case "Indiana": case "Indonesia": case "Inner Mongolia": case "Iowa": case "Iran": case "Iraq": 
+        case "Ireland": case "Isle of Man": case "Israel": case "Jamaica": case "Japan": case "Jiangsu": case "Jiangxi": 
+        case "Jilin": case "Jordan": case "Kansas": case "Kazakhstan": case "Kentucky": case "Kenya": case "Korea, South": 
+        case "Kuwait": case "Kyrgyzstan": case "Latvia": case "Lazio": case "Lebanon": case "Liaoning": case "Liberia": 
+        case "Libya": case "Liguria": case "Lithuania": case "Lombardia": case "Louisiana": case "Luxembourg": case "Macau": 
+        case "Madagascar": case "Maine": case "Malawi": case "Malaysia": case "Mali": case "Manitoba": case "Maranhao": 
+        case "Marche": case "Maryland": case "Massachusetts": case "Mato Grosso": case "Mato Grosso Do Sul": case "Mauritania": 
+        case "Mauritius": case "Mexico": case "Michigan": case "Minas Gerais": case "Minnesota": case "Mississippi": 
+        case "Missouri": case "Moldova": case "Molise": case "Mongolia": case "Montana": case "Montserrat": case "Morocco": 
+        case "Mozambique": case "Namibia": case "Nebraska": case "Nepal": case "Netherlands": case "Nevada": 
+        case "New Brunswick": case "New Caledonia": case "New Hampshire": case "New Jersey": case "New Mexico": 
+        case "New South Wales": case "New York": case "New Zealand": case "Newfoundland and Labrador": case "Nicaragua": 
+        case "Niger": case "Nigeria": case "Ningxia": case "North Carolina": case "North Dakota": case "Northern Territory": 
+        case "Northwest Territories": case "Norway": case "Nova Scotia": case "Ohio": case "Oklahoma": case "Oman": 
+        case "Ontario": case "Oregon": case "P.A. Bolzano": case "P.A. Trento": case "Pakistan": case "Panama": 
+        case "Papua New Guinea": case "Para": case "Paraguay": case "Paraiba": case "Parana": case "Pennsylvania": 
+        case "Pernambuco": case "Peru": case "Philippines": case "Piaui": case "Piemonte": case "Poland": case "Portugal": 
+        case "Prince Edward Island": case "Puglia": case "Qatar": case "Qinghai": case "Quebec": case "Queensland": 
+        case "Reunion": case "Rio De Janeiro": case "Rio Grande Do Norte": case "Rio Grande Do Sul": case "Romania": 
+        case "Rondonia": case "Roraima": case "Russia": case "Rwanda": case "Saint Barthelemy": case "Saint Pierre and Miquelon": 
+        case "Santa Catarina": case "Sao Paulo": case "Sardegna": case "Saskatchewan": case "Saudi Arabia": case "Senegal": 
+        case "Serbia": case "Sergipe": case "Seychelles": case "Shaanxi": case "Shandong": case "Shanghai": case "Shanxi": 
+        case "Sichuan": case "Sicilia": case "Sierra Leone": case "Singapore": case "Sint Maarten": case "Slovakia": 
+        case "Slovenia": case "Somalia": case "South Africa": case "South Australia": case "South Carolina": case "South Dakota": 
+        case "South Sudan": case "Spain": case "Sri Lanka": case "Sudan": case "Suriname": case "Sweden": case "Switzerland": 
+        case "Syria": case "Taiwan": case "Tanzania": case "Tasmania": case "Tennessee": case "Texas": case "Thailand": 
+        case "Tianjin": case "Tibet": case "Timor-Leste": case "Tocantins": case "Togo": case "Toscana": 
+        case "Trinidad and Tobago": case "Tunisia": case "Turkey": case "Turks and Caicos Islands": case "Uganda": 
+        case "Ukraine": case "Umbria": case "United Arab Emirates": case "United Kingdom": case "Uruguay": case "Utah": 
+        case "Uzbekistan": case "Valle d'Aosta": case "Veneto": case "Venezuela": case "Vermont": case "Victoria": case "Vietnam": 
+        case "Virginia": case "Washington": case "West Virginia": case "Western Australia": case "Wisconsin": case "Wyoming": 
+        case "Xinjiang": case "Yukon": case "Yunnan": case "Zambia": case "Zhejiang": case "Zimbabwe":
 
             return {
                 ...state,
