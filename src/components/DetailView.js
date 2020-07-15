d8ca18a56e9d1fc1a4d5c22116e47fa72f0cb7a0
daibi07 <1047776488@qq.com>
2020-07-15 15:14:09 +0800

    增加图表的控制栏

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -2,193 +2,396 @@ import React, {useContext} from 'react';
 import {store} from "../store";
 import ReactEcharts from "echarts-for-react";
 
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
-function Detailview() {
-    const {state} = useContext(store);
+import AcUnitIcon from '@material-ui/icons/AcUnit';
+import WavesIcon from '@material-ui/icons/Waves';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 
-    const getOption = () => {
-            if (state.left != null){
-                // 计算温度的最低值，将其作为Y轴下限
-                let TempArray = state.temp[0].slice(state.left, state.right).map(Number)
-                let TempArray1 = []
-                // 建了一个新数组TempArray1来保存原数组中非0的数
-                for(var i in TempArray){
-                    if(TempArray[i]!==0){
-                        TempArray1.push(TempArray[i]);
-                    } }
-                let TempMin = Math.min(...TempArray1) -3 //减去3是为了不让数据点和x轴重合
-                
-                return {
-                    title: {
-                        text: 'Relationship between new cases and temperature',
-                        subtext: 'XXXXX',
-                        left: 'center'
-                    },
-                    tooltip: {
-                        trigger: 'axis',
-                        axisPointer: {
-                            animation: false
-                        }
-                    },
-                    legend: {
-                        data: ['temperature', 'new'],
-                        left: 10
-                    },
-                    toolbox: {
-                        feature: {
-                            dataZoom: {
-                                yAxisIndex: 'none'
-                            },
-                            restore: {},
-                            saveAsImage: {}
-                        }
-                    },
-                    axisPointer: {
-                        link: {xAxisIndex: 'all'}
-                    },
-                    dataZoom: [
-                        {
-                            show: true,
-                            realtime: true,
-                            start: 40,
-                            end: 60,
-                            xAxisIndex: [0, 1]
-                        },
-                        {
-                            type: 'inside',
-                            realtime: true,
-                            start: 40,
-                            end: 70,
-                            xAxisIndex: [0, 1]
-                        }
-                    ],
-                    grid: [{
-                        left: 50,
-                        right: 50,
-                        height: '35%'
-                    }, {
-                        left: 50,
-                        right: 50,
-                        top: '55%',
-                        height: '35%'
-                    }],
-                    xAxis: [
-                        {
-                            type: 'category',
-                            boundaryGap: false,
-                            axisLine: {onZero: true},
-                            data: state.date[0].slice(state.left, state.right).map(function (str) {
-                                return str.replace('/2020', '');})
-                        },
-                        {
-                            gridIndex: 1,
-                            type: 'category',
-                            boundaryGap: false,
-                            axisLine: {onZero: true},
-                            data: state.date[0].slice(state.left, state.right).map(function (str) {
-                                return str.replace('/2020', '');}),
-                            position: 'top'
-                        }
-                    ],
-                    yAxis: [
-                        {
-                            name: 'temperature(K)',
-                            type: 'value',
-                            // max: 310,
-                            min: TempMin,
-                        },
-                        {
-                            gridIndex: 1,
-                            name: 'new',
-                            type: 'value',
-                            inverse: true,
-                            // max: 1000,
-                            
-                        }
-                    ],
-                    series: [
-                        {
-                            name: 'temperature',
-                            type: 'line',
-                            symbolSize: 8,
-                            hoverAnimation: false,
-                            data: state.temp[0].slice(state.left, state.right)
-                        },
-                        {
-                            name: 'new',
-                            type: 'line',
-                            xAxisIndex: 1,
-                            yAxisIndex: 1,
-                            symbolSize: 8,
-                            hoverAnimation: false,
-                            data: state.new[0].slice(state.left, state.right)
-                        }
-                    ]
-                }
-            }
-            else{
-                return {
-                    xAxis: {
-                        type: 'category',
-                        data: [1, 2, 3]
-                    },
-                    yAxis: {
-                        type: 'value'
-                    },
-                    series: [{
-                        data: [1, 2, 3],
-                        type: 'line'
-                    }]
-                }
-            }
-    };
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
+import ThumbDown from '@material-ui/icons/ThumbDown';
+import ThumbUp from '@material-ui/icons/ThumbUp';
 
 
-    return <ReactEcharts option={getOption()}/>;
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-force-tabpanel-${index}`}
+      aria-labelledby={`scrollable-force-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
 
-export default Detailview;
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
 
-// function Detailview() {
-//   const {state} = useContext(store);
+function a11yProps(index) {
+  return {
+    id: `scrollable-force-tab-${index}`,
+    'aria-controls': `scrollable-force-tabpanel-${index}`,
+  };
+}
 
-//   const getOption = () => {
-//     let data_ = [];
-//           if (state.left != null){
-//             for (let index = state.left; index <= state.right; index++) {
-//               if (state.new[0][index] < 0)state.new[0][index] = 0;
-//               data_[index-state.left] = [state.uvb[0][index], state.new[0][index]];
-//             }
-//               return {
-//                 xAxis: {
-//                   scale: true
-//                 },
-//                 yAxis: {
-//                     scale: true
-//                 },
-//                 series: [{
-//                     type: 'scatter',
-//                     data: data_,
-//                 }]
-//               }
-//           }
-//           else{
-//               return {
-//                 xAxis: {
-//                     scale: true
-//                 },
-//                 yAxis: {
-//                     scale: true
-//                 },
-//                 series: [{
-//                     type: 'scatter',
-//                     data: [[161.2, 51.6], [167.5, 59.0], [159.5, 49.2], [157.0, 63.0], [155.8, 53.6]],
-//                 }]
-//             }
-//           }
-//   };
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '100%',
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
 
-//   return <ReactEcharts option={getOption()}/>;
-// >>>>>>> 61893fa914e4f78b82fcd1f836956e23495191ce
-// }
 
+
+
+
+
+export default function ScrollableTabsButtonForce() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  const {state} = useContext(store);
+//   温度的图表
+  const getOption1 = () => {
+    if (state.left != null){
+        // 计算温度的最低值，将其作为Y轴下限
+        let TempArray = state.temp[0].slice(state.left, state.right).map(Number)
+        let TempArray1 = []
+        // 建了一个新数组TempArray1来保存原数组中非0的数
+        for(var i in TempArray){
+            if(TempArray[i]!==0){
+                TempArray1.push(TempArray[i]);
+            } }
+        let TempMin = Math.min(...TempArray1) -3 //减去3是为了不让数据点和x轴重合
+        
+        return {
+            title: {
+                text: 'Relationship between new cases and temperature',
+                subtext: 'XXXXX',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    animation: false
+                }
+            },
+            legend: {
+                data: ['temperature', 'new'],
+                left: 10
+            },
+            toolbox: {
+                feature: {
+                    dataZoom: {
+                        yAxisIndex: 'none'
+                    },
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            axisPointer: {
+                link: {xAxisIndex: 'all'}
+            },
+            dataZoom: [
+                {
+                    show: true,
+                    realtime: true,
+                    start: 40,
+                    end: 60,
+                    xAxisIndex: [0, 1]
+                },
+                {
+                    type: 'inside',
+                    realtime: true,
+                    start: 40,
+                    end: 70,
+                    xAxisIndex: [0, 1]
+                }
+            ],
+            grid: [{
+                left: 50,
+                right: 50,
+                height: '35%'
+            }, {
+                left: 50,
+                right: 50,
+                top: '55%',
+                height: '35%'
+            }],
+            xAxis: [
+                {
+                    type: 'category',
+                    boundaryGap: false,
+                    axisLine: {onZero: true},
+                    data: state.date[0].slice(state.left, state.right).map(function (str) {
+                        return str.replace('/2020', '');})
+                },
+                {
+                    gridIndex: 1,
+                    type: 'category',
+                    boundaryGap: false,
+                    axisLine: {onZero: true},
+                    data: state.date[0].slice(state.left, state.right).map(function (str) {
+                        return str.replace('/2020', '');}),
+                    position: 'top'
+                }
+            ],
+            yAxis: [
+                {
+                    name: 'temperature(K)',
+                    type: 'value',
+                    // max: 310,
+                    min: TempMin,
+                },
+                {
+                    gridIndex: 1,
+                    name: 'new',
+                    type: 'value',
+                    inverse: true,
+                    // max: 1000,
+                    
+                }
+            ],
+            series: [
+                {
+                    name: 'temperature',
+                    type: 'line',
+                    symbolSize: 8,
+                    hoverAnimation: false,
+                    data: state.temp[0].slice(state.left, state.right)
+                },
+                {
+                    name: 'new',
+                    type: 'line',
+                    xAxisIndex: 1,
+                    yAxisIndex: 1,
+                    symbolSize: 8,
+                    hoverAnimation: false,
+                    data: state.new[0].slice(state.left, state.right)
+                }
+            ]
+        }
+    }
+    else{
+        return {
+            xAxis: {
+                type: 'category',
+                data: [1, 2, 3]
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [1, 2, 3],
+                type: 'line'
+            }]
+        }
+    }
+  };
+//   紫外线的图表
+  const getOption2 = () => {
+    if (state.left != null){
+        // 计算uvb的最低值，将其作为Y轴下限
+        let UvbArray = state.uvb[0].slice(state.left, state.right).map(Number)
+        let UvbArray1 = []
+        // 建了一个新数组UvbArray1来保存原数组中非0的数
+        for(var i in UvbArray){
+            if(UvbArray[i]!==0){
+                UvbArray1.push(UvbArray[i]);
+            } }
+        let TempMin = Math.min(...UvbArray1) -300 //减去300是为了不让数据点和x轴重合
+        
+        return {
+            title: {
+                text: 'Relationship between new cases and uvb',
+                subtext: 'XXXXX',
+                left: 'center'
+            },
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {
+                    animation: false
+                }
+            },
+            legend: {
+                data: ['uvb', 'new'],
+                left: 10
+            },
+            toolbox: {
+                feature: {
+                    dataZoom: {
+                        yAxisIndex: 'none'
+                    },
+                    restore: {},
+                    saveAsImage: {}
+                }
+            },
+            axisPointer: {
+                link: {xAxisIndex: 'all'}
+            },
+            dataZoom: [
+                {
+                    show: true,
+                    realtime: true,
+                    start: 40,
+                    end: 60,
+                    xAxisIndex: [0, 1]
+                },
+                {
+                    type: 'inside',
+                    realtime: true,
+                    start: 40,
+                    end: 70,
+                    xAxisIndex: [0, 1]
+                }
+            ],
+            grid: [{
+                left: 50,
+                right: 50,
+                height: '35%'
+            }, {
+                left: 50,
+                right: 50,
+                top: '55%',
+                height: '35%'
+            }],
+            xAxis: [
+                {
+                    type: 'category',
+                    boundaryGap: false,
+                    axisLine: {onZero: true},
+                    data: state.date[0].slice(state.left, state.right).map(function (str) {
+                        return str.replace('/2020', '');})
+                },
+                {
+                    gridIndex: 1,
+                    type: 'category',
+                    boundaryGap: false,
+                    axisLine: {onZero: true},
+                    data: state.date[0].slice(state.left, state.right).map(function (str) {
+                        return str.replace('/2020', '');}),
+                    position: 'top'
+                }
+            ],
+            yAxis: [
+                {
+                    name: 'uvb',
+                    type: 'value',
+                    // max: 310,
+                    min: TempMin,
+                },
+                {
+                    gridIndex: 1,
+                    name: 'new',
+                    type: 'value',
+                    inverse: true,
+                    // max: 1000,
+                    
+                }
+            ],
+            series: [
+                {
+                    name: 'uvb',
+                    type: 'line',
+                    symbolSize: 8,
+                    hoverAnimation: false,
+                    data: state.uvb[0].slice(state.left, state.right)
+                },
+                {
+                    name: 'new',
+                    type: 'line',
+                    xAxisIndex: 1,
+                    yAxisIndex: 1,
+                    symbolSize: 8,
+                    hoverAnimation: false,
+                    data: state.new[0].slice(state.left, state.right)
+                }
+            ]
+        }
+    }
+    else{
+        return {
+            xAxis: {
+                type: 'category',
+                data: [1, 2, 3]
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [1, 2, 3],
+                type: 'line'
+            }]
+        }
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons="on"
+          indicatorColor="primary"
+          textColor="primary"
+          aria-label="scrollable force tabs example"
+        >
+          <Tab label="Temperature" icon={<AcUnitIcon />} {...a11yProps(0)} />
+          <Tab label="UVB Value" icon={<WavesIcon />} {...a11yProps(1)} />
+          <Tab label="GDP" icon={<MonetizationOnIcon />} {...a11yProps(2)} />
+          <Tab label="Polulation" icon={<SupervisorAccountIcon />} {...a11yProps(3)} />
+          <Tab label="Item Five" icon={<ShoppingBasket />} {...a11yProps(4)} />
+          <Tab label="Item Six" icon={<ThumbDown />} {...a11yProps(5)} />
+          <Tab label="Item Seven" icon={<ThumbUp />} {...a11yProps(6)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <ReactEcharts option={getOption1()}/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <ReactEcharts option={getOption2()}/>
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Item Four
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Item Five
+      </TabPanel>
+      <TabPanel value={value} index={5}>
+        Item Six
+      </TabPanel>
+      <TabPanel value={value} index={6}>
+        Item Seven
+      </TabPanel>
+    </div>
+  );
+}
