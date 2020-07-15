3a14984aef9b00325b86289ee656de28f0444697
LiuNick2000 <2605159470@qq.com>
2020-07-15 20:53:32 +0800

    标题时间还缺地点，补一下

--- src/components/AssistView.js
+++ src/components/AssistView.js
@@ -1,11 +1,15 @@
 import React, {useContext} from 'react';
 import {store} from "../store";
 
+
 function AssistView() {
     const {state} = useContext(store);
     console.log(state.case)
     return <div>
+    <h3 font="">世界疫情数据可视化</h3>
     <p>2020.07.10</p>
+    <p>{state.region}</p>
+    console.log(state.region);
 </div>;
 }
 
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -44,7 +44,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
-    padding:0,
   },
 }));
 
